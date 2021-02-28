043c369d80e0dad1df3075b641c219f01d36b2b7
Naoki Nakamichi <ronnao0516@gmail.com>
2021-02-28 14:37:24 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    let hello = "Hello, world!\n\nこんにちは\n\nSep/16/2020\n";
+
+    hello
+}
+
+#[get("/page_a")]
+fn page_a() -> &'static str {
+    let page_a = "こんにちは";
+
+    page_a
+}
+
+
+#[get("/page_b")]
+fn page_b() -> &'static str {
+    let page_b = "今晩は";
+
+    page_b
+}
+
+
+fn main() {
+    rocket::ignite().mount("/", routes![index,page_a,page_b]).launch();
+}
